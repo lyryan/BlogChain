7423a9f4e0b033f9996c20e98dc2db9136196dc0
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2019-05-09 10:55:00 -0700

    Update index.js

--- src/client/components/article-list/index.js
+++ src/client/components/article-list/index.js
@@ -7,6 +7,9 @@ import './index.css';
 
 const artData = require('../../pages/article/articleInfo');
 
+/*
+ * ArticleList component renders the list of articles on a page
+*/
 class ArticleList extends React.Component {
   render() {
     return (
@@ -15,10 +18,16 @@ class ArticleList extends React.Component {
   }
 }
 
+/*
+ * The function reloading the page to its top
+*/
 function reload() {
   window.scrollTo(0, 0);
 }
 
+/*
+ * Component rendering each individual article on a page
+*/
 class Articles extends React.Component {
    constructor(props) {
        super(props);
@@ -30,7 +39,7 @@ class Articles extends React.Component {
            .then(response => this.setState({ articles: response.data} ))
            .catch(err => console.log(err));
    }
-
+   /* Use Material UI Card to render all information about an article*/
    render() {
    const style = {
         textDecoration: null,
@@ -40,55 +49,51 @@ class Articles extends React.Component {
         return (
             <div
             className = "root"
-            href = "/article" >
-             <div className = "child" >
+            href = "/article">
+             <div className = "child">
                 {state.map(function (article, i) {
                     if (article.title !== ar) {
                         return (
-                            <Card
-                            key = {article.title}
-                            className = "article" >
-                            <CardActionArea >
-                            <Link
-                              onClick = {reload}
-                              to = {{
+                            <Card key = {article.title}
+                            className="article">
+                            <CardActionArea>
+                            <Link onClick = {reload}
+                              to={{
                                pathname: "/article/${articleData[i].title}",
                                state: {title: article.title,
                                author: article.author,
                                date: article.date,
                                text: article.text,
                             }
-                        }
-                    }>
+                          }
+                        }>
                     <CardImg
                         top
                         width = "100%"
                         height = "90%"
                         src = "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" / >
                             <CardContent
-                        className="card-content" >
-                            <div
-                        className = "art-title"
-                        style = {style} >
-                            {article.title}
-                    </div>
-                        <div
-                        className = "art-author"
-                        style = {style} >
-                            {article.author}
-                    </div>
+                              className="card-content" >
+                             <div className = "art-title"
+                               style={style}>
+                               {article.title}
+                             </div>
+                             <div className = "art-author"
+                                style={style}>
+                                {article.author}
+                            </div>
                         </CardContent>
-                        </Link>
-                        </CardActionArea>
-                        </Card>
-                    )
-                    }
-                })
-        }
+                      </Link>
+                    </CardActionArea>
+                  </Card>
+                )
+              }
+           })
+         }
         </div>
-            </div>
-        );
-     }
+      </div>
+    );
+  }
 }
 
 export default ArticleList;
