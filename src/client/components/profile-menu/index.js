08affa80110f402970972f854e1ce5f7a67a4379
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2019-05-12 11:23:04 -0700

    Update index.js
    Comments

--- src/client/components/profile-menu/index.js
+++ src/client/components/profile-menu/index.js
@@ -3,13 +3,12 @@ import { IconButton } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
+/*
+ * Component rendering Profile Menu icon allowing users to log in or go to their Profile page
+ */
 class ProfileMenu extends React.Component {
-
-
   render() {
-
     const Profile = props => <Link to="/profile" {...props} />
-
     return (
       <div>
         <IconButton
