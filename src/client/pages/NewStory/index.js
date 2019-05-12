d81ff6a36409702f5da29795200f4759bc1709e4
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2019-05-12 12:13:00 -0700

    Update index.js
    Comment

--- src/client/pages/NewStory/index.js
+++ src/client/pages/NewStory/index.js
@@ -5,6 +5,7 @@ import TextEditor from '../../components/text-editor';
  * This components renders the page for writing and posting a new article
 */
 const NewStory = ({ address }) => (
+      // return a text editor component
       <TextEditor/>
   );
 export default NewStory;
