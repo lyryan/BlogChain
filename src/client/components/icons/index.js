a1fd9ee16a2ea7d2f46ad934ad2a267809cb655d
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2019-05-12 12:27:21 -0700

    Update index.js
    Comments

--- src/client/components/icons/index.js
+++ src/client/components/icons/index.js
@@ -9,21 +9,25 @@ import {
  */
 const Icon = () => (
   <div className="icons">
+    // icon for sending an article via email
     <div className="email" title="Email article" href="#">
       <EmailShareButton className="email-button" url="127.0.0.1:3000/article">
         <EmailIcon className="email-icon" size={34} round />
       </EmailShareButton>
     </div>
+    // icon for sharing an article on Facebook
     <div className="facebook" title="Share on Facebook" href="#">
       <FacebookShareButton className="facebook-button" quote="Test article" url="127.0.0.1:3000/article">
         <FacebookIcon className="facebook-icon" size={34} round />
       </FacebookShareButton>
     </div>
+    // icon for sharing an article on Twitter
     <div className="twitter" title="Share on Twitter" href="#">
       <TwitterShareButton className="twitter-button" url="http://127.0.0.1:3000/article">
         <TwitterIcon className="twitter-icon" size={34} round />
       </TwitterShareButton>
     </div>
+    // icon for sharing an article on Linkedin
     <div className="linkedin" title="Share on Linkedin" href="#">
       <LinkedinShareButton className="linkedin-button" url="http://127.0.0.1:3000/article">
         <LinkedinIcon className="linkedin-icon" size={34} round />
